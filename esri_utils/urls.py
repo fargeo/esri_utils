f3df098e345bf65f7fc39ca672b836265b1fd293
njkim <nkim@fargeo.com>
2021-06-01 20:28:46 -0700

    Add parcel overlay map from postgres table, #12

--- esri_utils/urls.py
+++ esri_utils/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from esri_utils.views.overlays import MVT_Parcels, Geojson_Parcels
+
 
 urlpatterns = [
     url(r'^', include('arches.urls')),
+    url(
+        r"^mvt-parcels/(?P<zoom>[0-9]+|\{z\})/(?P<x>[0-9]+|\{x\})/(?P<y>[0-9]+|\{y\}).pbf$",
+        MVT_Parcels.as_view(), name="mvt-parcels",
+    ),
+    url(r"^geojson-parcel/(?P<selectedApn>.*)$", Geojson_Parcels.as_view(), name="geojson_parcel"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
